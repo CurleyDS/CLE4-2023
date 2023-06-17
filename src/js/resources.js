b29151911d73e80e8bd91fde154a6d4367744600
Dylan Schoop <42898108+CurleyDS@users.noreply.github.com>
2023-06-18 00:52:43 +0200

    Remove last starter-code

--- src/js/resources.js
+++ src/js/resources.js
@@ -1,12 +1,10 @@
 import { ImageSource, Sound, Resource, Loader } from 'excalibur'
 import { TiledMapResource } from '@excaliburjs/plugin-tiled'
-import fishImage from '../images/fish.png'
 import playerCharacter from '../images/spritesheet-player-character.png'
 
 const Resources = {
-    Fish: new ImageSource(fishImage),
     Player: new ImageSource(playerCharacter)
 }
-const ResourceLoader = new Loader([Resources.Fish, Resources.Player])
+const ResourceLoader = new Loader([Resources.Player])
 
 export { Resources, ResourceLoader }
