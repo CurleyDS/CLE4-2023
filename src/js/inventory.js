7ea3b0673806537efb715b1fe64f1a0f27c9ab5a
Dylan Schoop <42898108+CurleyDS@users.noreply.github.com>
2023-06-23 10:15:00 +0200

    added functionality in inventory

--- src/js/inventory.js
+++ src/js/inventory.js
@@ -9,13 +9,21 @@ export class Inventory {
 
     addToInventory(item) {
         // check of item al bestaat
-        if (this.items.find(element => element.name == item.name)) {
+        if (this.items.find(item => item.name == item.name)) {
             // voeg toe aan aantal van huidige item
             const itemIndex = this.items.findIndex(element => element.name == item.name)
             this.items[itemIndex].amount++
         } else {
             // voeg nieuwe item toe
             this.items.push(item)
+        }
+    }
+
+    hasItem(itemName) {
+        if (this.items.find(item => item.name == itemName)) {
+            return true
+        } else {
+            return false
         }
     }
 }
